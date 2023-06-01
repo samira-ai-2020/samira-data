fc93894010b9fac9c65b8e9e7625b21ba6cc7ef0
Samira <samira-ai-2020>
2023-06-02 01:21:49 +0330

    Getting input from and printing table with from library

--- prc-2.py
+++ prc-2.py
@@ -1,3 +1,4 @@
+# گرفتن ورودی از کاربر و چاپ جدول با استفاده از کتابخانه
 from tabulate import tabulate  
 from collections import Counter  
 print(tabulate([[char, count] for char, count in Counter(input("Please Enter Text: ").replace(" ","")).items()], headers=["name", "frequency"], tablefmt="grid"))
